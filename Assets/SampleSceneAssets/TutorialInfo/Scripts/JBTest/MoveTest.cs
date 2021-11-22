0830233bf00d1edb2511dc4bfb9918024b96b763
ErbakAsrevnel <jeanbaptistemaupin@gmail.com>
2021-11-22 10:17:56 +0100

    Plus de 8 directions pour le controller

--- Assets/SampleSceneAssets/TutorialInfo/Scripts/JBTest/MoveTest.cs
+++ Assets/SampleSceneAssets/TutorialInfo/Scripts/JBTest/MoveTest.cs
@@ -31,8 +31,8 @@ public class MoveTest : MonoBehaviour
     // Valeures pour le déplacement des personnages.
 
     // Les inputs Normalisés
-    public int NormInputX { get; private set; }
-    public int NormInputY { get; private set; }
+    public float NormInputX { get; private set; }
+    public float NormInputY { get; private set; }
 
     [Header("Valeur de vitesse de côté")]
     [Space(10)]
@@ -52,26 +52,27 @@ public class MoveTest : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         Vector2 rawMoveInput = context.ReadValue<Vector2>();
-        Debug.Log(rawMoveInput);
+        //Debug.Log(rawMoveInput);
         rbCharacter.velocity = rawMoveInput * strafeSpeed;
-        if (Mathf.Abs(rawMoveInput.x) > 0.5f)
+        if (Mathf.Abs(rawMoveInput.x) > 0.1f)
         {
-            NormInputX = (int)(rawMoveInput * Vector2.right).normalized.x;
+            NormInputX = (rawMoveInput * Vector2.right).x;
         }
         else
         {
             NormInputX = 0;
         }
 
-        if (Mathf.Abs(rawMoveInput.y) > 0.5f)
+        if (Mathf.Abs(rawMoveInput.y) > 0.1f)
         {
-            NormInputY = (int)(rawMoveInput * Vector2.up).normalized.y;
+            NormInputY = (rawMoveInput * Vector2.up).y;
         }
         else
         {
             NormInputY = 0;
         }
         Vector2 moveInput = new Vector2(NormInputX,NormInputY);
+        Debug.Log(moveInput);
         rbCharacter.velocity = moveInput * strafeSpeed;
     }
 
@@ -82,7 +83,7 @@ public class MoveTest : MonoBehaviour
         Debug.Log("Ohlala on va vite");
         if(context.performed)
         {
-        playerParent.m_Speed = boostTrailSpeed;
+            playerParent.m_Speed = boostTrailSpeed;
         }
         else{
             playerParent.m_Speed = trailSpeed;

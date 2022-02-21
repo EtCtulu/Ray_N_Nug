a80480976315d5c80ed2aec1dad12b6831ecc3e4
jojo8210 <joseph.pelloquet@gmail.com>
2022-02-21 09:35:37 +0100

    Base sound manager

---
+++ Assets/SampleSceneAssets/Scripts/SoundManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static SoundManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+}
